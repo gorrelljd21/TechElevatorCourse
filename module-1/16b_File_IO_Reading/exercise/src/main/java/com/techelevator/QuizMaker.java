8359ad5ae9eadbdae40d32f8e46a63404478e3a3
Jessie Gorrell <gorrelljd21@gmail.com>
2022-06-13 12:59:22 -0400

    M2E1

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,24 +1,76 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class QuizMaker {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-		//reading file
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the fully qualified name of the file to read in for quiz questions:");
+        String filePath = scanner.nextLine();
 
+
+        List<QuizQuestion> questions = new ArrayList<>();
+
+        File testFile = new File(filePath);
+        try (Scanner fileName = new Scanner(testFile)) {
+            while (fileName.hasNextLine()) {
+                String line = fileName.nextLine();
+                questions.add(new QuizQuestion(line));
+            }
+        }
+        int correctCount = 0;
+        for (QuizQuestion question : questions) {
+            System.out.printf("%s\n", question.getQuestion());
+            for (String availOption : question.getOptions()) {
+                System.out.println(availOption);
+            }
+            System.out.println("What is your answer?" );
+            String userAnswer = scanner.nextLine();
+            if(userAnswer.equalsIgnoreCase(question.getAnswer())) {
+                correctCount = correctCount + 1;
+
+                System.out.println("You are correct");
+                System.out.printf("Correct answers %s \n", correctCount);
+
+            } else {
+                System.out.println("You are incorrect");
+            }
+
+        }
 
 
     }
 }
 
+
+//        String[] parts = "one |two |three".split( "\\|");
+//        for (String part : parts) {
+//            System.out.println(part);
+// > one
+// > two
+// > three
+
+// get question
+// print question
+// print options, add 1-4, subtract 1 for correct index, .compare()
+//
+
+
 // reading question file
 // while has next line
 // split the string
 //List<QuizQuestions>
-    //collection
+//collection
 
 // presenting questions to user
 //presenting questions
 //metrics tracking
+
+//System.out.println("What is your answer?" );
+//            String userAnswer = scanner.nextLine();
--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizQuestion.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizQuestion.java
@@ -1,9 +1,41 @@
 package com.techelevator;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuizQuestion extends QuizMaker{
+public class QuizQuestion {
+
+    private String question;
+    private List<String> options = new ArrayList<>();
+    private String answer = "";
+
+    public QuizQuestion(String question){
+        String[] questionOptions = question.split("\\|");
+        this.question = questionOptions[0];
+        for(int i = 1; i < questionOptions.length; i++){
+            if(questionOptions[i].contains("*")){
+                String noStar = questionOptions[i].replace("*", "");
+                this.answer = noStar;
+                this.options.add(noStar);
+            } else {
+                this.options.add(questionOptions[i]);
+            }
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
 
 }
 
